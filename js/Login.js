e9ec04ea84657cdf1880bf36d6edc9608e554825
ziad nasser <ziadnasser896@gmail.com>
2023-04-15 13:48:44 +0200

    JS code for login & registerNewAdmin

---
+++ js/Login.js
@@ -0,0 +1,23 @@
+const emailInput = document.getElementById('email');
+const passwordInput = document.getElementById('password');
+const loginForm = document.querySelector('form');
+
+const admins = JSON.parse(localStorage.getItem('admins')) || [];
+
+loginForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  
+  const email = emailInput.value;
+  const password = passwordInput.value;
+
+  const admin = admins.find(admin => admin.email === email && admin.password === password);
+
+  if (admin) {
+    alert('Login successful!');
+    window.location.href = 'home.html';
+  } else {
+    alert('Invalid email or password.');
+  }
+
+  localStorage.setItem('admins', JSON.stringify(admins));
+});
