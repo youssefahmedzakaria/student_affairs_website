7bfc7b9de3fc241a89fadb240dc229ace3044b4b
YoussefMahmoudOksha <http://youssef.mahmoud75359@gmail.com/>
2023-05-22 19:10:14 +0300

    add js file

--- js/RistrictedPages.js
+++ js/RistrictedPages.js
@@ -26,39 +26,51 @@ function displayLoggedInPages() {
     while (nav.firstChild) {
         nav.firstChild.remove();
     }
-    var homeLItem = document.createElement("li");
-    homeLItem.appendChild(document.createTextNode("Home"));
+    var homeLItem = document.createElement("i");
+    homeLItem.setAttribute("class", "material-icons");
+    homeLItem.appendChild(document.createTextNode("home"));
 
     var homeAnchor = document.createElement("a");
     homeAnchor.href = "home.html";
+    homeAnchor.setAttribute("class", "menu__item");
+    homeAnchor.setAttribute("data-tooltip", "Home");
     homeAnchor.id = "home";
     homeAnchor.appendChild(homeLItem);
     nav.appendChild(homeAnchor);
 
-    var dashBoardLItem = document.createElement("li");
-    dashBoardLItem.appendChild(document.createTextNode("Dash Board"));
+    var dashBoardLItem = document.createElement("i");
+    dashBoardLItem.setAttribute("class", "material-icons");
+    dashBoardLItem.appendChild(document.createTextNode("dashboard"));
 
     var dashBoardAnchor = document.createElement("a");
     dashBoardAnchor.href = "DashBoard.html";
+    dashBoardAnchor.setAttribute("class", "menu__item");
+    dashBoardAnchor.setAttribute("data-tooltip", "DashBoard");
     dashBoardAnchor.id = "dashboard";
     dashBoardAnchor.appendChild(dashBoardLItem);
     nav.appendChild(dashBoardAnchor);
 
-    var aboutUsLItem = document.createElement("li");
-    aboutUsLItem.appendChild(document.createTextNode("About Us"));
+    var aboutUsLItem = document.createElement("i");
+    aboutUsLItem.setAttribute("class", "material-icons");
+    aboutUsLItem.appendChild(document.createTextNode("groups"));
 
     var aboutUsAnchor = document.createElement("a");
     aboutUsAnchor.href = "AboutUs.html";
+    aboutUsAnchor.setAttribute("class", "menu__item");
+    aboutUsAnchor.setAttribute("data-tooltip", "About Us");
     aboutUsAnchor.id = "about";
     aboutUsAnchor.appendChild(aboutUsLItem);
     nav.appendChild(aboutUsAnchor);
 
-    var logOutLItem = document.createElement("li");
-    logOutLItem.appendChild(document.createTextNode("Log Out"));
+    var logOutLItem = document.createElement("i");
+    logOutLItem.setAttribute("class", "material-icons");
+    logOutLItem.appendChild(document.createTextNode("logout"));
     logOutLItem.id = "LogOut";
 
     var logOutAnchor = document.createElement("a");
     logOutAnchor.href = "home.html";
+    logOutAnchor.setAttribute("class", "menu__item");
+    logOutAnchor.setAttribute("data-tooltip", "Log Out");
     logOutAnchor.appendChild(logOutLItem);
     nav.appendChild(logOutAnchor);
 }
@@ -68,29 +80,39 @@ function hideLoggedOutPages() {
         nav.firstChild.remove();
     }
 
-    var homeLItem = document.createElement("li");
-    homeLItem.appendChild(document.createTextNode("Home"));
+    var homeLItem = document.createElement("i");
+    homeLItem.setAttribute("class", "material-icons");
+    homeLItem.appendChild(document.createTextNode("home"));
 
     var homeAnchor = document.createElement("a");
     homeAnchor.href = "home.html";
+    homeAnchor.setAttribute("class", "menu__item");
+    homeAnchor.setAttribute("data-tooltip", "Home");
     homeAnchor.id = "home";
     homeAnchor.appendChild(homeLItem);
     nav.appendChild(homeAnchor);
 
-    var aboutUsLItem = document.createElement("li");
-    aboutUsLItem.appendChild(document.createTextNode("About Us"));
+    var aboutUsLItem = document.createElement("i");
+    aboutUsLItem.setAttribute("class", "material-icons");
+    aboutUsLItem.appendChild(document.createTextNode("groups"));
+    
 
     var aboutUsAnchor = document.createElement("a");
     aboutUsAnchor.href = "AboutUs.html";
+    aboutUsAnchor.setAttribute("class", "menu__item");
+    aboutUsAnchor.setAttribute("data-tooltip", "About Us");
     aboutUsAnchor.id = "about";
     aboutUsAnchor.appendChild(aboutUsLItem);
     nav.appendChild(aboutUsAnchor);
 
-    var loginLItem = document.createElement("li");
-    loginLItem.appendChild(document.createTextNode("Log In"));
+    var loginLItem = document.createElement("i");
+    loginLItem.setAttribute("class", "material-icons");
+    loginLItem.appendChild(document.createTextNode("login"));
 
     var loginAnchor = document.createElement("a");
     loginAnchor.href = "Login.html";
+    loginAnchor.setAttribute("class", "menu__item");
+    loginAnchor.setAttribute("data-tooltip", "Log In");
     loginAnchor.id = "login";
     loginAnchor.appendChild(loginLItem);
     nav.appendChild(loginAnchor);
